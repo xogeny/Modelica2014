d3dcb4bdf58bd9392074db253f0e45ad1e6bba7a
Michael Tiller <michael.tiller@gmail.com>
2014-01-27 16:30:23 -0500

    Trying to match up schedule with easychair

--- process.py
+++ process.py
@@ -1,6 +1,19 @@
 #!/bin/env python
 
 import json
+import xml.etree.ElementTree as ET
+
+tree = ET.parse('easychair.xml')
+submissions = tree.find("submissions").iter('submission')
+title_to_num = {}
+subs_by_num = {}
+
+for sub in submissions:
+    num = sub.find('number').text
+    title = sub.find('title').text.strip()
+    abstract = sub.find('abstract').text
+    title_to_num[title] = num
+    subs_by_num[num] = {"title": title, "abstract": abstract}
 
 fp = open("raw.data", "r")
 program = []
@@ -19,10 +32,17 @@ for line in lines:
         slot["sessions"].append(session)
     if line.startswith("*** "):
         pdata = line.strip()[4:].split(".")
-        paper = {"authors": pdata[0], "title": ".".join(pdata[1:])}
+        authors = pdata[0]
+        title = ".".join(pdata[1:]).strip()
+        num = title_to_num.get(title, "")
+        if num=="":
+            print "Couldn't find number for: '%s'" % (title,)
+        paper = {"authors": authors, "title": title}
         session["papers"].append(paper)
 
 data = {"program": program}
+
+# print str(subs_by_num)
 
 fp = open("data.json", "w")
 json.dump(data, fp, indent=2)
