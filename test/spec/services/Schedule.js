7357d8513c4e6b004af9ce74259360b308d1e6fb
Michael Tiller <michael.tiller@gmail.com>
2014-01-27 18:34:55 -0500

    Refining the schedule model

--- test/spec/services/Schedule.js
+++ test/spec/services/Schedule.js
@@ -15,8 +15,19 @@ describe('Service: Schedule', function () {
       expect("program" in Schedule);
   });
 
+  it("first day of program should be Sunday", function () {
+      expect(Schedule["program"][0]["day"]=="Sunday");
+  });
+
+  it("second day of program should be Monday", function () {
+      expect(Schedule["program"][1]["day"]=="Monday");
+  });
+
+  it("third day of program should be Tuesday", function () {
+      expect(Schedule["program"][2]["day"]=="Tuesday");
+  });
+
   it("should have a papers array", function () {
       expect("papers" in Schedule);
   });
-
 });
