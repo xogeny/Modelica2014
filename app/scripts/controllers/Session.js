cc7237b2a2d6c72e78369dcd355c8a58e904edf9
Michael Tiller <michael.tiller@gmail.com>
2014-01-28 17:21:57 -0500

    Nice tooltip and improved switch button

--- app/scripts/controllers/Session.js
+++ app/scripts/controllers/Session.js
@@ -7,11 +7,19 @@ angular.module('AppMod2014App')
       $scope.papers = Schedule["papers"]
       $scope.userdata = Userdata
       $scope.slot = $routeParams.id[0]+$routeParams.id[1];
+      $scope.selected = Userdata['choices'][$scope.slot];
       $scope.searchTerm = topicFilter.current;
       $scope.$watch('searchTerm', function (nv, ov) {
 	  topicFilter.current = nv;
       });
       $scope.showPaper = function(pnum) {
 	  return topicFilter["checkPaper"](pnum, topicFilter.current.toLowerCase());
+      };
+      if ($scope.selected!=null) {
+	  var fullname = $scope.slot+"-"+$scope.selected;
+	  $('#switch').tooltip({
+	      'placement': "left",
+	      'title': "Current session: "+Schedule["sessions"][fullname].name
+	  });
       }
   });
