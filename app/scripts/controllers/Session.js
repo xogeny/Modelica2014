bd9f1fe3e7be1284c20c16189573b0d8c66ea267
Michael Tiller <michael.tiller@gmail.com>
2014-01-28 13:05:34 -0500

    Added a nice view of the papers (including abstracts)

--- app/scripts/controllers/Session.js
+++ app/scripts/controllers/Session.js
@@ -3,4 +3,5 @@
 angular.module('AppMod2014App')
   .controller('SessionCtrl', function ($scope, $routeParams, Schedule) {
     $scope.session = Schedule["sessions"][$routeParams.id];
+    $scope.papers = Schedule["papers"]
   });
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -3,9 +3,4 @@
 angular.module('AppMod2014App')
   .controller('MainCtrl', function ($scope, Schedule) {
       $scope.schedule = Schedule;
-      $scope.awesomeThings = [
-	  'HTML5 Boilerplate',
-	  'AngularJS',
-	  'Karma'
-      ];
   });
