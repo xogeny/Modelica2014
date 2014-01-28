7947dc18abf3369d0d5d0b9a71d2ad7bf9a28546
Michael Tiller <michael.tiller@gmail.com>
2014-01-28 12:02:47 -0500

    About to refactor the schedule.json file
    I want to pull all the session information out into its own hash.

--- app/scripts/controllers/Session.js
+++ app/scripts/controllers/Session.js
@@ -1,6 +1,6 @@
 'use strict';
 
 angular.module('AppMod2014App')
-  .controller('SessionCtrl', function ($scope, $routeParams) {
-    $scope.session = $routeParams.id;
+  .controller('SessionCtrl', function ($scope, $routeParams, Schedule) {
+    $scope.session_id = $routeParams.id;
   });
