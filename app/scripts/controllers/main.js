7e61a11d83229b0db5da0e953ac776a5cc1fbad3
Michael Tiller <michael.tiller@gmail.com>
2014-01-29 18:37:14 -0500

    Got a connection to localStorage working
    There were some really strange bugs in Firefox that really slowed me down here.  But I think
    I've got something that could be a nice, generic approach for local storage.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -3,7 +3,26 @@
 angular.module('AppMod2014App')
   .controller('MainCtrl', function ($scope, Schedule, Userdata, topicFilter) {
       $scope.schedule = Schedule;
-      $scope.userdata = Userdata;
+      //$scope.choices = {"T1": "B", "T2": "A"};
+      $scope.choices = Userdata.get("choices");
+
+      /* This is enormously ugly but it was the only way I found that I could
+	 work around an issue with Firefox not reliably detecting changes...ARGH!
+	 (see more below) */
+      $scope.$watch(function() { 
+	  return angular.toJson($scope.choices);
+      }, function(nv, ov) {
+	  //console.log("Choices STRING changed from "+ov+" to "+nv);
+	  Userdata.set("choices", $scope.choices);
+      });
+
+      /* This SHOULD work, but doesn't (work reliably in Firefox) */
+      /*
+      $scope.$watch("choices", function (nv, ov) {
+	  console.log("Choices changed from "+ov+" to "+nv);
+      }, true);
+      */
+
       $scope.searchTerm = topicFilter.current;
       $scope.$watch('searchTerm', function (nv, ov) {
 	  topicFilter.current = nv;
--- app/scripts/services/UserData.js
+++ app/scripts/services/UserData.js
@@ -1,13 +1,54 @@
 'use strict';
 
+function mod2014_reset() {
+    localStorage.clear();
+}
+
 angular.module('AppMod2014App')
   .service('Userdata', function Userdata() {
+      var baseData = {
+	  "version": 1,
+	  "choices": {"T1": null, "T2": null, "T3": null, "T4": null,
+		      "W1": null, "W2": null, "W3": null, "W4": null}
+      };
+      var init = angular.fromJson(localStorage.mod2014);
+      console.log("mod2014 = ");
+      console.log(init);
+
+      if (init===undefined) {
+	  console.log("Initializing mod2014 data");
+	  localStorage.mod2014 = angular.toJson(baseData);
+      } else if (init['version']!=baseData['version']) {
+	  console.log("Resetting mod2014 data because of a new version");
+	  localStorage.mod2014 = angular.toJson(baseData);	  
+      }
+      var foo = {"choices": {"T1": "B", "T2": "A"}};
+      return {
+	  set: function(k, v) {
+	      console.log("Setting "+k+" to be "+v);
+	      var data = angular.fromJson(localStorage.getItem("mod2014"));
+	      //foo[k] = v;
+	      data[k] = v;
+	      localStorage.setItem("mod2014", angular.toJson(data));
+	      return v;
+	  },
+	  get: function(k) {
+	      //return foo[k];
+	      return angular.fromJson(localStorage.getItem("mod2014"))[k];
+	  }
+      };
+      /*
+      var init = angular.fromJson(localStorage.mod2014);
+      console.log("mod2014 = ");
+      console.log(init);
+
+      if (init===undefined) {
+	  localStorage.mod2014 = angular.toJson(baseData);
+      }
       // Ultimately, this should be tied to some local database
       // (perhaps via JayData?).  We should try to detect if
       // local data is available and, if not, initialize that
       // data
-      return {
-	  "choices": {"T1": null, "T2": null, "T3": null, "T4": null,
-		      "W1": null, "W2": null, "W3": null, "W4": null}
-      }
+      return baseData;
+      */
   });
